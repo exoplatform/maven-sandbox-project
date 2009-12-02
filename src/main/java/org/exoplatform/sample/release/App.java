53c9609f5968dead6980c3ed7022a21bcab5e05d
Arnaud  Héritier <arnaud.heritier@exoplatform.com>
2009-12-02 15:01:12 +0000

    Basic project created from an archetype and which extends our parent 1.2

---
+++ src/main/java/org/exoplatform/sample/release/App.java
@@ -0,0 +1,13 @@
+package org.exoplatform.sample.release;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
